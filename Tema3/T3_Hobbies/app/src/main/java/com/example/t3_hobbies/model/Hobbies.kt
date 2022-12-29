e95769938ea641120fe0da9b9de42d982a669155
DevSys-M <81291792+DevSys-M@users.noreply.github.com>
2022-12-29 20:40:54 +0100

    update

---
+++ Tema3/T3_Hobbies/app/src/main/java/com/example/t3_hobbies/model/Hobbies.kt
@@ -0,0 +1,20 @@
+package com.example.t3_hobbies.model
+
+import java.io.Serializable
+
+class Hobbies(private var nombre: String,private var detalle: String,private var imagen: Int,private var categoria: String): Serializable {
+
+    fun getNombre(): String{
+        return this.nombre
+    }
+    fun getDetalle(): String{
+        return this.detalle
+    }
+    fun getImagen(): Int{
+        return this.imagen
+    }
+    fun getCategoria(): String{
+        return this.categoria
+    }
+
+}
