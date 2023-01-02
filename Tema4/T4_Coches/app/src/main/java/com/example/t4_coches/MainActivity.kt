35bc2db20875bf0f017b7ee67dde14082f205a4f
DevSys-M <81291792+DevSys-M@users.noreply.github.com>
2023-01-02 19:42:02 +0100

    update

--- Tema4/T4_Coches/app/src/main/java/com/example/t4_coches/MainActivity.kt
+++ Tema4/T4_Coches/app/src/main/java/com/example/t4_coches/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.t4_coches
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.t4_coches.databinding.ActivityMainBinding
+import com.example.t4_coches.model.Coche
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var listaCoches: ArrayList<Coche>
+
+    // spinner filtrar
+    private lateinit var precio: String
+    private lateinit var marca: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
@@ -25,10 +33,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rellenarDatos() {
-        TODO("Not yet implemented")
+        listaCoches = ArrayList();
+
+        listaCoches.add(Coche("Mercedes", "AMG GT", 500, 200000, "Deportivo", R.drawable.amggt))
+        listaCoches.add(Coche("Bentley", "Continental", 400, 300000, "Berlina deportivo", R.drawable.continental))
+        listaCoches.add(Coche("Jaguar", "FType", 300, 150000, "Deportivo", R.drawable.ftype))
+        listaCoches.add(Coche("Ford", "GT40", 500, 300000, "Deportivo clasico", R.drawable.gt40))
+        listaCoches.add(Coche("Nissan", "GTR", 300, 200000, "Deportivo", R.drawable.gtr))
+        listaCoches.add(Coche("Porche", "Huayara", 600, 400000, "Deportivo", R.drawable.huayra))
+        listaCoches.add(Coche("Lexus", "LC", 200, 100000, "Deportivo", R.drawable.lc))
+        listaCoches.add(Coche("Ferrari", "Le ferrari", 600, 600000, "Deportivo", R.drawable.leferrari))
+        listaCoches.add(Coche("McLaren", "MC600", 500, 450000, "Deportivo", R.drawable.mc600))
+        listaCoches.add(Coche("Toyota", "Supra", 300, 150000, "Deportivo", R.drawable.supra))
+        listaCoches.add(Coche("Porche", "Taycan", 350, 250000, "Deportivo", R.drawable.taycan))
+        //adaptadorCoche = AdaptadorCoche(this, listaCoches)
     }
 
     private fun instancias() {
-        TODO("Not yet implemented")
+        binding.spinnerMarca.adapter = ArrayAdapter.createFromResource(applicationContext, R.array.lista_marcas, android.R.layout.simple_spinner_item)
+        (binding.spinnerMarca.adapter as ArrayAdapter<CharSequence>).setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        binding.spinnerPrecio.adapter = ArrayAdapter.createFromResource(applicationContext, R.array.lista_precios, android.R.layout.simple_spinner_item)
+        (binding.spinnerPrecio.adapter as ArrayAdapter<CharSequence>).setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
     }
 }
--- Tema4/T4_Coches/app/src/main/java/com/example/t4_coches/model/Coche.kt
+++ Tema4/T4_Coches/app/src/main/java/com/example/t4_coches/model/Coche.kt
@@ -2,4 +2,4 @@ package com.example.t4_coches.model
 
 import java.io.Serializable
 
-data class Coches(var marca: String, var modelo: String, var cv: Int, var precio: Int, var tipo: String, var imagen: Int): Serializable
+data class Coche (var marca: String, var modelo: String, var cv: Int, var precio: Int, var tipo: String, var imagen: Int): Serializable
