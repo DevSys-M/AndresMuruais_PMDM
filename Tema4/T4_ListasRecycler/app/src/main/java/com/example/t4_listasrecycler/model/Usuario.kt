f49956308ed698b7928eebe25fb5a820da97b950
DevSys-M <81291792+DevSys-M@users.noreply.github.com>
2023-01-02 22:30:47 +0100

    update

---
+++ Tema4/T4_ListasRecycler/app/src/main/java/com/example/t4_listasrecycler/model/Usuario.kt
@@ -0,0 +1,6 @@
+package com.example.t4_listasrecycler.model
+
+import java.io.Serializable
+
+data class Usuario (var nombre: String, var apellido: String, var telefono: Int, var imagen: Int):
+    Serializable
