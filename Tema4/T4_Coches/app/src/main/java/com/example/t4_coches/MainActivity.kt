f3f1b9b570de6262382e40c85f3198e46b78a411
DevSys-M <81291792+DevSys-M@users.noreply.github.com>
2023-01-02 19:15:38 +0100

    update

--- Tema4/T4_Coches/app/src/main/java/com/example/t4_coches/MainActivity.kt
+++ Tema4/T4_Coches/app/src/main/java/com/example/t4_coches/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.t4_coches
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.t4_coches.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        instancias()
+        rellenarDatos()
+        asociarDatos()
+        acciones()
+    }
+
+    private fun acciones() {
+        TODO("Not yet implemented")
+    }
+
+    private fun asociarDatos() {
+        TODO("Not yet implemented")
+    }
+
+    private fun rellenarDatos() {
+        TODO("Not yet implemented")
+    }
+
+    private fun instancias() {
+        TODO("Not yet implemented")
     }
 }
