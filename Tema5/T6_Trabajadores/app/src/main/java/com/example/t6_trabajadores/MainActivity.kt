f06e184493dbaea2468a057f5044d82a6c8d831a
DevSys-M <81291792+DevSys-M@users.noreply.github.com>
2023-01-29 22:02:45 +0100

    update

--- Tema5/T6_Trabajadores/app/src/main/java/com/example/t6_trabajadores/MainActivity.kt
+++ Tema5/T6_Trabajadores/app/src/main/java/com/example/t6_trabajadores/MainActivity.kt
@@ -2,28 +2,57 @@ package com.example.t6_trabajadores
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.t6_trabajadores.adapters.AdapterTrabajador
 import com.example.t6_trabajadores.databinding.ActivityMainBinding
+import com.example.t6_trabajadores.dialogs.DialogoComunica
+import com.example.t6_trabajadores.model.Trabajador
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),AdapterTrabajador.OnRecyclerListener {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var adaptadorTrabajadores: AdapterTrabajador
+    private lateinit var trabajador: Trabajador
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         instancias()
-        rellenarDatos()
         acciones()
     }
 
     private fun instancias() {
-        TODO("Not yet implemented")
+        adaptadorTrabajadores = AdapterTrabajador(this, ArrayList<Trabajador>())
+        binding.recycler.adapter= adaptadorTrabajadores
+        binding.recycler.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        binding.spinnerPuesto.adapter =   ArrayAdapter.createFromResource(this,R.array.puestos,android.R.layout.simple_spinner_item)
+        (binding.spinnerPuesto.adapter as ArrayAdapter<CharSequence>).setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
     }
 
-    private fun rellenarDatos() {
-        TODO("Not yet implemented")
-    }
 
     private fun acciones() {
-        TODO("Not yet implemented")
+       binding.botonAdd.setOnClickListener{
+           trabajador = Trabajador(
+               binding.editNombre.text.toString(),
+               binding.editApellido.text.toString(),
+               binding.editCorreo.text.toString(),
+               binding.editEdad.text.toString(),
+               binding.spinnerPuesto.selectedItem.toString()
+           )
+           adaptadorTrabajadores.addTrabajador(trabajador)
+       }
+    }
+
+    override fun onRecyclerSelected(trabajador: Trabajador) {
+        DialogoComunica.newInstance(trabajador).show(supportFragmentManager,null)
+        binding.editNombre.setText("")
+        binding.editApellido.setText("")
+        binding.editCorreo.setText("")
+        binding.editEdad.setText("")
+        binding.spinnerPuesto.setSelection(0)
     }
 }
--- Tema5/T6_Trabajadores/app/src/main/java/com/example/t6_trabajadores/model/Trabajador.kt
+++ Tema5/T6_Trabajadores/app/src/main/java/com/example/t6_trabajadores/model/Trabajador.kt
@@ -1,43 +1,12 @@
 package com.example.t6_trabajadores.model
 
-import android.os.Parcel
-import android.os.Parcelable
+
+import java.io.Serializable
 
 data class Trabajador(
-    var nombre: String?,
-    var apellidos: String?,
-    var correo: String?,
-    var edad: Int,
-    var puesto: String?
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readInt(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(nombre)
-        parcel.writeString(apellidos)
-        parcel.writeString(correo)
-        parcel.writeInt(edad)
-        parcel.writeString(puesto)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Trabajador> {
-        override fun createFromParcel(parcel: Parcel): Trabajador {
-            return Trabajador(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Trabajador?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    var nombre: String,
+    var apellidos: String,
+    var correo: String,
+    var edad: String,
+    var puesto: String
+) : Serializable
