92bd42d96ee12c5ce4f5777bf48514a78c7cab13
DevSys-M <81291792+DevSys-M@users.noreply.github.com>
2023-02-19 18:50:52 +0100

    update

--- Tema7/T7_IncioFragment_Dos/app/src/main/java/com/example/t7_inciofragment_dos/MainActivity.kt
+++ Tema7/T7_IncioFragment_Dos/app/src/main/java/com/example/t7_inciofragment_dos/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.t7_inciofragment_dos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.t7_inciofragment_dos.databinding.ActivityMainBinding
+import com.example.t7_inciofragment_dos.fragments.FragmentDos
+import com.example.t7_inciofragment_dos.fragments.FragmentUno
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener, FragmentUno.OnFragmentNombreListener {
+
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.botonF1.setOnClickListener( this)
+        binding.botonF2.setOnClickListener( this)
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0!!.id){
+            binding.botonF1.id ->{
+                val ft = supportFragmentManager.beginTransaction();
+                ft.replace(binding.sitioFragments.id,FragmentUno())
+                ft.commit()
+            }
+            binding.botonF2.id ->{
+
+                val ft = supportFragmentManager.beginTransaction();
+                ft.replace(binding.sitioFragments.id, FragmentDos())
+                ft.commit()
+            }
+            binding.botonF3.id ->{
+
+                val fm = supportFragmentManager
+
+            }
+        }
+    }
+
+
+    override fun onNombreSelected(nombre: String) {
+        // TODO 4. Ejecuto el cambio de fragment, con newInstance
+        val fm = supportFragmentManager;
+        val ft = fm.beginTransaction();
+        ft.replace(binding.sitioFragments.id,FragmentDos.newInstance(nombre))
+        ft.commit()
+        Snackbar.make(binding.root,nombre,Snackbar.LENGTH_SHORT).show()
     }
 }
--- Tema7/T7_IncioFragment_Dos/app/src/main/java/com/example/t7_inciofragment_dos/fragments/FragmentHeader.kt
+++ Tema7/T7_IncioFragment_Dos/app/src/main/java/com/example/t7_inciofragment_dos/fragments/FragmentHeader.kt
@@ -1,4 +1,41 @@
 package com.example.t7_inciofragment_dos.fragments
 
-class FragmentHeader {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import com.example.t7_inciofragment_dos.databinding.FragmentHeaderBinding
+
+class FragmentHeader: Fragment() {
+
+    private lateinit var binding: FragmentHeaderBinding;
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHeaderBinding.inflate(inflater,container,false)
+        return binding.root;
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.botonHeader.setOnClickListener {
+            //Snackbar.make(binding.root,"Primera app con fragments",Snackbar.LENGTH_SHORT).show()
+            crearDialogoAyuda().show()
+        }
+
+    }
+
+
+    fun crearDialogoAyuda(): AlertDialog {
+        val builder : AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        builder.setMessage("Primera aplicacion con fragments")
+        return builder.create();
+
+    }
+
 }
