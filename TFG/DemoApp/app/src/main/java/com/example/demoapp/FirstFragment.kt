da95a3e285e57f83e7c113af620777d0ff38f0a2
DevSys-M <81291792+DevSys-M@users.noreply.github.com>
2023-05-24 18:52:21 +0200

    update

--- TFG/DemoApp/app/src/main/java/com/example/demoapp/FirstFragment.kt
+++ TFG/DemoApp/app/src/main/java/com/example/demoapp/FirstFragment.kt
@@ -1,12 +1,12 @@
 package com.example.demoapp
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.demoapp.adapter.AdaptadorCalendario
 import com.example.demoapp.databinding.FragmentFirstBinding
